d774599ba4f3b9440936e5cc19a6a0211031a577
SaKsHaTGaRg <sakshat2422@gmail.com>
2025-11-26 01:48:01 -0500

    updated method

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,5 +1,5 @@
 // Clear JWT token and redirect user to login page
-export const signOutUser = () => {
+export const logout = () => {
   localStorage.removeItem("token"); // remove stored auth token
   window.location.href = "/login";  // force redirect
 };
